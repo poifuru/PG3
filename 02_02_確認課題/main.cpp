4d16bea2803b59c0045c17bd7b815ed14cc532fe
剛 増谷 <K024G0075@g.neec.ac.jp>
2025-10-24 22:23:25 +0900

    2-2修正完了

--- 02_02_確認課題/main.cpp
+++ 02_02_確認課題/main.cpp
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
-//再帰関数
-int Recursion(int num) {
-	int newNum = 0;
-	newNum = num * 2 - 50;
-	if(newNum >= 999999) {
-		return newNum;
-	}
-
-	return Recursion(newNum);
-}
-
-//比較して出力する関数
+// 比較して出力する関数
 void Comparation(int num, int num2) {
-	int i = 1;
-	printf("給料比較\n");
-	while(num * i > Recursion(num2) * i) {
-		printf("労働時間 %d時間  一般給料 : 時給%d円  トータル%d円\n", i, num, num * i);
-		printf("労働時間 %d時間  再帰給料 : 時給%d円  トータル%d円\n\n", i, Recursion(num2), Recursion(num2) * i);
+	int i = 1; // 労働時間
+	int total = 0;	//通常時給のトータル
+	int recTotal = 0;
+
+	printf("--- 給料比較---\n");
+
+	while(recTotal <= total) {
+
+		//トータル給料を計算するやんす
+		total = num * i;
+		//結果を出力する
+		printf("労働時間 %d時間:\n", i);
+
+		printf("一般給の時給: %d円, トータル: %d円\n", num, total);
+
+		num2 = num2 * 2 - 50;
+		recTotal += num2;
+		printf("再帰的な時給: %d円, トータル: %d円\n\n", num2, recTotal);
+
 		i++;
 	}
 }
 
 int main() {
-	//一般給料
-	const int salary = 1226;
-	//再帰関数の給料
+	// 一般給料
+	int normalSalary = 1226;
+	// 再帰関数の給料の初期値
 	int recursionSalary = 100;
 
-	Comparation(salary, recursionSalary);
-
+	Comparation(normalSalary, recursionSalary);
+	
 	return 0;
 }
