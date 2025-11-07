3cc5821b5a99134aa87ab4f7643c2671bde14aa9
剛 増谷 <K024G0075@g.neec.ac.jp>
2025-11-07 23:32:34 +0900

    02_03完

--- 02_03_確認課題/main.cpp
+++ 02_03_確認課題/main.cpp
@@ -2,9 +2,44 @@
 #include <Windows.h>
 #include <random>
 
+void ShowResult(int roll, int userGuess);
+
+void DelayReveal(void(*fn)(int, int), unsigned int delayMs, int roll, int userGuess);
+
 int main() {
-	unsigned int currentTime = time(nullptr);
-	srand(currentTime);
+	//乱数の初期化
+	srand((unsigned)time(NULL));
+	int userGuess = 0;
+
+	//選択
+	printf("半か丁か選んでください\n");
+	scanf_s("%d", &userGuess);
+
+	//出目の生成
+	int roll = rand() % 6 + 1;
+
+	//関数ポインタ
+	void (*pfunc)(int, int);
+	pfunc = ShowResult;
+
+	//判定
+	DelayReveal(pfunc, 3000, roll, userGuess);
 
 	return 0;
+}
+
+void ShowResult(int roll, int userGuess) {
+	printf("出目は%dでした\n", roll);
+	int judge = roll % 2;
+	if(judge == 0 && userGuess == 0 || judge == 1 && userGuess == 1) {
+		printf("正解\n");
+	}
+	else {
+		printf("不正解\n");
+	}
+}
+
+void DelayReveal(void(*fn)(int, int), unsigned int delayMs, int roll, int userGuess) {
+	Sleep(delayMs);
+	fn(roll, userGuess);
 }
