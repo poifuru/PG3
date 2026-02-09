31b8362e4e900a40e707c4ee1413b63433645ebc
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-02-09 12:29:07 +0900

    03_02修正完了

--- 03_02/Min.h
+++ 03_02/Min.h
@@ -2,22 +2,23 @@
 #include <stdio.h>
 
 template <typename T1, typename T2>
-class Min {
+class MinCalculator {
 public:
 	T1 num1_;
 	T2 num2_;
 
-	Min(T1 num1, T2 num2) {
+	MinCalculator(T1 num1, T2 num2) {
 		num1_ = num1;
 		num2_ = num2;
-	};
+	}
 
-	void Comparison() {
+	// 戻り値の型は、より精度の高い方や計算結果に合うよう auto（あるいは適切な型）にすると柔軟
+	auto Min() {
 		if (num1_ < num2_) {
-			printf("%f\n", static_cast<float>(num1_));
+			return static_cast<double>(num1_);
 		}
 		else {
-			printf("%f\n", static_cast<float>(num2_));
+			return static_cast<double>(num2_);
 		}
-	};
+	}
 };
--- 03_02/main.cpp
+++ 03_02/main.cpp
@@ -2,16 +2,21 @@
 #include <memory>
 
 int main() {
-	std::unique_ptr<Min<int, int>>min1 = std::make_unique<Min<int, int>>(1, 22);
-	min1->Comparison();
-	std::unique_ptr<Min<float, float>>min2 = std::make_unique<Min<float, float>>(1.2f, 1.5f);
-	min2->Comparison();
-	std::unique_ptr<Min<double, double>>min3 = std::make_unique<Min<double, double>>(1.8, 1.5);
-	min3->Comparison();
-	std::unique_ptr<Min<int, float>>min4 = std::make_unique<Min<int, float>>(1, 1.5f);
-	min4->Comparison();
-	std::unique_ptr<Min<float, double>>min5 = std::make_unique<Min<float, double>>(2.0f, 1.5);
-	min5->Comparison();
-	std::unique_ptr<Min<double, int>>min6 = std::make_unique<Min<double, int>>(0.3, 1.5f);
-	min6->Comparison();
+	std::unique_ptr<MinCalculator<int, int>> min1 = std::make_unique<MinCalculator<int, int>>(1, 22);
+	printf("%f\n", static_cast<float>(min1->Min()));
+
+	std::unique_ptr<MinCalculator<float, float>> min2 = std::make_unique<MinCalculator<float, float>>(1.2f, 1.5f);
+	printf("%f\n", static_cast<float>(min2->Min()));
+
+	std::unique_ptr<MinCalculator<double, double>> min3 = std::make_unique<MinCalculator<double, double>>(1.8, 1.5);
+	printf("%f\n", static_cast<float>(min3->Min()));
+
+	std::unique_ptr<MinCalculator<int, float>> min4 = std::make_unique<MinCalculator<int, float>>(1, 1.5f);
+	printf("%f\n", static_cast<float>(min4->Min()));
+
+	std::unique_ptr<MinCalculator<float, double>> min5 = std::make_unique<MinCalculator<float, double>>(2.0f, 1.5);
+	printf("%f\n", static_cast<float>(min5->Min()));
+
+	std::unique_ptr<MinCalculator<double, int>> min6 = std::make_unique<MinCalculator<double, int>>(0.3, 1.5f);
+	printf("%f\n", static_cast<float>(min6->Min()));
 }
